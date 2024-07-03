ae2e4f591575235b1e5fb80718f01420f6d2735b
falola <pharlorlah700@gmail.com>
2024-07-03 02:23:16 +0100

    formatted

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const currentTimeElement = document.querySelector('[data-testid="currentTimeUTC"]');
-    const currentDayElement = document.querySelector('[data-testid="currentDay"]');
-    
-    function updateTimeAndDay() {
-        const now = new Date();
-       
+document.addEventListener("DOMContentLoaded", function () {
+  const currentTimeElement = document.querySelector(
+    '[data-testid="currentTimeUTC"]'
+  );
+  const currentDayElement = document.querySelector(
+    '[data-testid="currentDay"]'
+  );
 
-        const options = { weekday: 'long' };
-        
-        // Format time in HH:MM:SS AM/PM format
-        let hours = now.getHours();
-        const minutes = now.getMinutes().toString().padStart(2, '0');
-        const seconds = now.getSeconds().toString().padStart(2, '0');
-        const ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12;
-        hours = hours ? hours : 12; // The hour '0' should be '12'
-        const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
-        
-        // Display current time and day
-        currentTimeElement.textContent = `${timeString} UTC+1`;
-        currentDayElement.textContent = new Intl.DateTimeFormat('en-GB', options).format(now);
-    }
+  function updateTimeAndDay() {
+    const now = new Date();
 
-    updateTimeAndDay();
-    setInterval(updateTimeAndDay, 1000); // Update every second
+    const options = { weekday: "long" };
+
+    // Format time in HH:MM:SS AM/PM format
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // The hour '0' should be '12'
+    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+    // Display current time and day
+    currentTimeElement.textContent = `${timeString} UTC+1`;
+    currentDayElement.textContent = new Intl.DateTimeFormat(
+      "en-GB",
+      options
+    ).format(now);
+  }
+
+  updateTimeAndDay();
+  setInterval(updateTimeAndDay, 1000); // Update every second
 });
